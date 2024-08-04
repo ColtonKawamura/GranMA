e74cfb2046f9c94fe3dc6bfc78581f4d1db180f0
Colton Kawamura <colton.kawamura1@nps.edu>
2024-08-04 06:16:51 -0400

    more initital commits and edits

--- test_script.jl
+++ test_script.jl
@@ -5,8 +5,10 @@ using CSV
 using DataFrames
 
 # Read the data table
-data_frame = CSV.read("out/test_crunch.csv", DataFrame)
+data_frame = CSV.read("out/processed_simulation/2d_test_crunch.csv", DataFrame)
 
 plot_ellipse_pressure_2d(data_frame, .1)
 
 simulation_2d(100, 1, .1, 1, 5000, .1, 5, 5)
+
+plot_attenuation(data_frame, .1)
