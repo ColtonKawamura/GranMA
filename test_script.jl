d4fc6a02db4cf737c92146829431a1cf6f450d8c
Colton Kawamura <colton.kawamura1@nps.edu>
2024-08-04 20:00:09 -0400

    added GLMakie plot options Native to Julia

--- test_script.jl
+++ test_script.jl
@@ -7,7 +7,7 @@ using DataFrames
 # Read the data table
 data_frame = CSV.read("out/processed_simulation/K100_ellipse_edits.csv", DataFrame)
 
-plot_ellipse_ωγ_2d(data_frame, .1)
+plot_ellipse_ωγ_2d(data_frame, .1, 1)
 
 simulation_2d(100, 1, .1, 1, 5000, .1, 5, 5)
 
