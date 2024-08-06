1c99b29ad38a13284f6e456f9c68dba87708ca07
Colton Kawamura <colton.kawamura1@nps.edu>
2024-08-06 05:49:36 -0700

    intial commit for HPC

--- packing_job_2d_bi.sh
+++ packing_job_2d_bi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=2D_packing_job
+#SBATCH --job-name=packing_job_2d_bi
 #SBATCH --array=1-20%20
 #SBATCH --nodes=4
 #SBATCH --ntasks=4
 #SBATCH --time=72:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=colton.kawamura1@nps.edu
-#SBATCH -o packing_job.out
+#SBATCH -o packing_job_2d_bi.out
 #SBATCH --mem-per-cpu=8000
 
 source /etc/profile
@@ -16,4 +16,4 @@ module use /share/modules/base
 module load compile/gcc/7.2.0
 module load app/matlab/R2023b
 
-sed -n "${SLURM_ARRAY_TASK_ID}p" packing_job_list.txt | /bin/bash
+sed -n "${SLURM_ARRAY_TASK_ID}p" packing_job_2d_bi.txt | /bin/bash
