df3f1fcf05db76b993cfdc72666f59624860f63f
Colton Kawamura <colton.kawamura1@nps.edu>
2024-08-07 19:07:49 -0400

    Testing to see what happens if plots move over to sandbox

--- sandbox.jl
+++ sandbox.jl
@@ -8,6 +8,112 @@ using DataFrames
 data_frame = CSV.read("out/processed_simulation/K100_ellipse_edits.csv", DataFrame)
 data_frame = CSV.read("out/processed_simulation/2d_K100_ellipse_edits.csv", DataFrame)
 
+
+
+
+function plot_ellipse_pdf(simulation_data, ω_value, γ_value)
+
+    # filter the data based on those that are close to gamma_value
+    closest_γ_index = argmin(abs.([idx.gamma for idx in simulation_data] .- γ_value))
+    closest_γ_value = simulation_data[closest_γ_index].gamma
+    matching_γ_data = filter(entry -> entry.gamma == closest_γ_value, simulation_data)
+
+    # filter the data based on those that are close to gamma_value
+    closest_ω_index = argmin(abs.([idx.omega for idx in matching_γ_data] .- ω_value))
+    closest_ω_value = matching_γ_data[closest_ω_index].omega
+    matching_ωγ_data = filter(entry -> entry.omega == closest_ω_value, matching_γ_data)
+
+    # Get a list of unique input pressures
+    pressure_list = unique([entry.pressure for entry in matching_ωγ_data]) # goes through each entry of simulation_data and get the P value at that entry
+    
+    # get a range for plotting color from 0 to 1
+    normalized_variable = (log.(pressure_list) .- minimum(log.(pressure_list))) ./ (maximum(log.(pressure_list)) .- minimum(log.(pressure_list)))
+
+    # Intialized the plots to loop over
+    mat"""
+    figure_aspect_ratio = figure;
+    xlabel('\$ b/a \$', "FontSize", 20, "Interpreter", "latex");
+    ylabel('\$ p(b/a) \$', "FontSize", 20, "Interpreter", "latex");
+    set(gca, 'YScale', 'log');
+    set(get(gca, 'ylabel'), 'rotation', 0);
+    grid on;
+    box on;
+    hold on
+
+    figure_rotation_angle = figure;
+    xlabel('\$ \\theta \$', "FontSize", 20, "Interpreter", "latex");
+    ylabel('\$ p(\\theta) \$', "FontSize", 20, "Interpreter", "latex");
+    set(gca, 'YScale', 'log');
+    set(get(gca, 'ylabel'), 'rotation', 0);
+    grid on;
+    box on;
+    hold on
+    """
+
+    # Create a line for each pressure
+    for pressure_value in pressure_list
+
+        # Only look at data for current pressure value
+        matching_pressure_data = filter(entry -> entry.pressure == pressure_value, matching_ωγ_data) # for every entry in simluation_data, replace (->) that entry with result of the boolean expression
+        
+        # Assign a color
+        idx = findfirst(idx -> idx ==pressure_value, pressure_list) # find the first index that matches
+        marker_color = [normalized_variable[idx], 0, 1-normalized_variable[idx]]
+        
+        # Each omega gamma value spans all seeds **** can get rd
+        # ωγ_list = unique([entry.omega_gamma for entry in matching_pressure_data])
+
+
+        # mean(matrix, dims=2) means across rows. Dims = 1 is cross columns, dims =3 is into the screen
+        #  If you have an array [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]], splatting converts this into calling hcat([a1, a2, a3], [b1, b2, b3], [c1, c2, c3]).
+        mean_asp_rat_counts = mean(hcat([entry.asp_rat_counts for entry in matching_pressure_data]...), dims=2)
+        mean_asp_rat_bins = mean(hcat([entry.asp_rat_bins for entry in matching_pressure_data]...), dims=2)
+        mean_rot_ang_counts = mean(hcat([entry.rot_ang_counts for entry in matching_pressure_data]...), dims=2)
+        mean_rot_ang_bins = mean(hcat([entry.rot_ang_bins for entry in matching_pressure_data]...), dims=2)
+
+        @bp
+        
+        # Calculate bin widths
+        asp_rat_bin_widths = diff(mean_asp_rat_bins, dims=1)
+        rot_ang_bin_widths = diff(mean_rot_ang_bins, dims=1)
+
+        # Normalize counts to get probabilities, considering the bin widths
+        total_asp_count = sum(mean_asp_rat_counts .* asp_rat_bin_widths)
+        total_rot_count = sum(mean_rot_ang_counts .* rot_ang_bin_widths)
+        
+        probabilities_asp = (mean_asp_rat_counts ./ total_asp_count) ./ asp_rat_bin_widths
+        probabilities_rot = (mean_rot_ang_counts ./ total_rot_count) ./ rot_ang_bin_widths
+        plot_bins_asp = mean_asp_rat_bins[1:end-1]
+        plot_bins_rot = mean_rot_ang_bins[1:end-1]
+
+        # This is needed because MATLAB.jl has a hard time escaping \'s
+        pressure_label = @sprintf("\$ \\hat{P} = %.3f, \\hat{\\gamma} = %.3f , \\hat{\\omega} = %.3f \$", pressure_value, γ_value, ω_value)
+
+        mat"""
+        mean_asp_rat_counts = $(probabilities_asp);
+        mean_asp_rat_bins = $(plot_bins_asp);
+        mean_rot_ang_counts = $(probabilities_rot);
+        mean_rot_ang_bins = $(plot_bins_rot);
+        marker_color = $(marker_color);
+        pressure_label = $(pressure_label);
+        
+        figure(figure_aspect_ratio);
+        plot(mean_asp_rat_bins, mean_asp_rat_counts, '-o','MarkerFaceColor', marker_color, 'Color', marker_color, 'DisplayName', pressure_label);
+
+        figure(figure_rotation_angle);
+        plot(mean_rot_ang_bins, mean_rot_ang_counts, '-o','MarkerFaceColor', marker_color, 'Color', marker_color, 'DisplayName', pressure_label);
+        """
+    end
+    mat"""
+    figure(figure_aspect_ratio);
+    legend('show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
+
+    figure(figure_rotation_angle);
+    legend('show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
+    """
+end
+
+
 plot_ellipse_ωγ_2d(data_frame, .5,1)
 
 simulation_2d(100, 1, .1, 1, 5000, .1, 5, 5)
