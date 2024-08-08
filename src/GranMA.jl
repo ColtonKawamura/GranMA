6d9b529bc1a54f760b4ee5a80b66d65286ce1b5c
Colton Kawamura <colton.kawamura1@nps.edu>
2024-08-08 06:23:39 -0400

    added alpha over omega

--- src/GranMA.jl
+++ src/GranMA.jl
@@ -12,7 +12,7 @@ using MAT
 using Glob
 using JLD2
 
-export load_data2, file_data, crunch_and_save, crunch, save_data, load_data, plot_ellipse_ωγ_2d, random_aspect_ratio_check_2d, simulation_2d, plot_ωγ_attenuation_2d, plot_ωγ_wavespeed_2d, pack_poly_2d, plot_ellipse_low_pressure, plot_ellipse_pdf, process_outputs_2d
+export save_data2, load_data2, file_data, plot_ellipse_pdf, crunch_and_save, crunch, save_data, load_data, plot_ellipse_ωγ_2d, random_aspect_ratio_check_2d, simulation_2d, plot_ωγ_attenuation_2d, plot_ωγ_wavespeed_2d, pack_poly_2d, plot_ellipse_low_pressure, plot_ellipse_pdf, process_outputs_2d
 
 mutable struct file_data
     pressure::Float64
@@ -35,6 +35,8 @@ mutable struct file_data
     ellipse_stats::Matrix{Float64}
     fft_limit_y::Float64
     wavenumber_y::Float64
+    alphaoveromega_x::Float64
+    alphaoveromega_y::Float64
 end
 
 function crunch_and_save()
@@ -88,7 +90,6 @@ function crunch()
         # Handle potentially empty arrays for fft limits
         fft_x = get(iloop_file_data, "initial_distance_from_oscillation_output_x_fft", [])
         fft_y = get(iloop_file_data, "initial_distance_from_oscillation_output_y_fft", [])
-
         fft_limit_x = isempty(fft_x) ? NaN : maximum(fft_x)
         fft_limit_y = isempty(fft_y) ? NaN : maximum(fft_y)
 
@@ -112,7 +113,9 @@ function crunch()
             fft_limit_x,
             iloop_file_data["ellipse_stats_nonzero"],
             fft_limit_y,
-            wavenumber_y
+            wavenumber_y,
+            -attenuation_x / omega,
+            -attenuation_y / omega
         )
 
         push!(simulation_data, data_entry)
@@ -122,29 +125,29 @@ function crunch()
 end
 
 function save_data(simulation_data::Vector{file_data}, K::Int)
-    file_name = "out/processed/2d_K$(K).jld2"
+    file_name = "out/processed/2d_K$(K)_alpha.jld2"
     @save file_name simulation_data
     println("Data saved to $file_name")
 end
 
 function load_data(K::Int)::Vector{file_data}
-    file_name = "out/processed/2d_K$(K).jld2"
+    file_name = "out/processed/2d_K$(K)_alpha.jld2"
     @load file_name simulation_data
     return simulation_data
 end
 
-function load_data2(K::Int)::Vector{file_data}
-    file_name = "out/processed/2d_K$(K).jld2"
-    simulation_data = JLD2.load(file_name, "simulation_data")
+# function load_data2(K::Int)::Vector{file_data}
+#     file_name = "out/processed/2d_K$(K).jld2"
+#     simulation_data = JLD2.load(file_name, "simulation_data")
     
-    # Convert to the correct type
-    return [file_data(d.pressure, d.omega, d.gamma, d.asp_rat_counts, d.asp_rat_bins,
-                      d.rot_ang_counts, d.rot_ang_bins, d.omega_gamma, d.seed,
-                      d.pressure_actual, d.attenuation_x, d.attenuation_y, d.wavespeed_x,
-                      d.wavenumber_x, d.mean_aspect_ratio, d.mean_rotation_angles,
-                      d.fft_limit_x, d.ellipse_stats, d.fft_limit_y, d.wavenumber_y)
-            for d in simulation_data]
-end
+#     # Convert to the correct type
+#     return [file_data(d.pressure, d.omega, d.gamma, d.asp_rat_counts, d.asp_rat_bins,
+#                       d.rot_ang_counts, d.rot_ang_bins, d.omega_gamma, d.seed,
+#                       d.pressure_actual, d.attenuation_x, d.attenuation_y, d.wavespeed_x,
+#                       d.wavenumber_x, d.mean_aspect_ratio, d.mean_rotation_angles,
+#                       d.fft_limit_x, d.ellipse_stats, d.fft_limit_y, d.wavenumber_y)
+#             for d in simulation_data]
+# end
 
 
 
@@ -222,6 +225,21 @@ function simulation_2d(K, M, Bv, w_D, N, P, W, seed)
 end
 
 
+"""
+Plots ωγ vs Ellipse Data
+
+# Arguments
+- `data_frame::DataFrame`: This is the output from the "process_outputs_2d.m" function You will need to load the data using "data_frame = CSV.read("out/processed_simulation/your_file_name.csv", DataFrame)
+.
+- `gamma_value::Float65`: γ value that you want to plot.
+- `flag::Any`: Add a third flag if you want to use twin axis.
+
+# Example Function
+plot_ellipse_ωγ_2d(data_frame, .1)
+
+# NOTE
+Need to ensure you have the corresponding input files in the ./in/ directory 
+"""
 function plot_ellipse_ωγ_2d(data_frame, gamma_value) # using MATLAB
 
     # Define parameters to plot
@@ -698,21 +716,7 @@ function plot_ωγ_attenuation_2d(data_frame, gamma_value, flag::Any) # using MA
     """
 end
 
-"""
-Plots ωγ vs c
 
-# Arguments
-- `data_frame::DataFrame`: This is the output from the "process_outputs_2d.m" function You will need to load the data using "data_frame = CSV.read("out/processed_simulation/your_file_name.csv", DataFrame)
-.
-- `gamma_value::Float65`: γ value that you want to plot.
-- `flag::Any`: Add a third flag if you want to use MATLAB to plot.
-
-# Example Function
-plot_ωγ_wavespeed_2d(data_frame, .1)
-
-# NOTE
-Need to ensure you have the corresponding input files in the ./in/ directory 
-"""
 function plot_ωγ_wavespeed_2d(data_frame, gamma_value)
 
     # Define parameters to plot
@@ -1035,6 +1039,108 @@ function plot_ellipse_low_pressure(data_frame, gamma_values, pressure_value)
     legend(ax_attenuation, 'show', 'Location', 'eastoutside', 'Interpreter', 'latex');
     %legend(ax_rotation, 'show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
     %legend(ax_aspect_ratio, 'show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
+    """
+end
+
+function plot_ellipse_pdf(simulation_data, ω_value, γ_value)
+
+    # filter the data based on those that are close to gamma_value
+    closest_γ_index = argmin(abs.([idx.gamma for idx in simulation_data] .- γ_value))
+    closest_γ_value = simulation_data[closest_γ_index].gamma
+    matching_γ_data = filter(entry -> entry.gamma == closest_γ_value, simulation_data)
+
+    # filter the data based on those that are close to gamma_value
+    closest_ω_index = argmin(abs.([idx.omega for idx in matching_γ_data] .- ω_value))
+    closest_ω_value = matching_γ_data[closest_ω_index].omega
+    matching_ωγ_data = filter(entry -> entry.omega == closest_ω_value, matching_γ_data)
+
+    # Get a list of unique input pressures
+    pressure_list = unique([entry.pressure for entry in matching_ωγ_data]) # goes through each entry of simulation_data and get the P value at that entry
+    
+    # get a range for plotting color from 0 to 1
+    normalized_variable = (log.(pressure_list) .- minimum(log.(pressure_list))) ./ (maximum(log.(pressure_list)) .- minimum(log.(pressure_list)))
+
+    # Intialized the plots to loop over
+    mat"""
+    figure_aspect_ratio = figure;
+    xlabel('\$ b/a \$', "FontSize", 20, "Interpreter", "latex");
+    ylabel('\$ p(b/a) \$', "FontSize", 20, "Interpreter", "latex");
+    set(gca, 'YScale', 'log');
+    set(get(gca, 'ylabel'), 'rotation', 0);
+    grid on;
+    box on;
+    hold on
+
+    figure_rotation_angle = figure;
+    xlabel('\$ \\theta \$', "FontSize", 20, "Interpreter", "latex");
+    ylabel('\$ p(\\theta) \$', "FontSize", 20, "Interpreter", "latex");
+    set(gca, 'YScale', 'log');
+    set(get(gca, 'ylabel'), 'rotation', 0);
+    grid on;
+    box on;
+    hold on
+    """
+
+    # Create a line for each pressure
+    for pressure_value in pressure_list
+
+        # Only look at data for current pressure value
+        matching_pressure_data = filter(entry -> entry.pressure == pressure_value, matching_ωγ_data) # for every entry in simluation_data, replace (->) that entry with result of the boolean expression
+        
+        # Assign a color
+        idx = findfirst(idx -> idx ==pressure_value, pressure_list) # find the first index that matches
+        marker_color = [normalized_variable[idx], 0, 1-normalized_variable[idx]]
+        
+        # Each omega gamma value spans all seeds **** can get rd
+        # ωγ_list = unique([entry.omega_gamma for entry in matching_pressure_data])
+
+
+        # mean(matrix, dims=2) means across rows. Dims = 1 is cross columns, dims =3 is into the screen
+        #  If you have an array [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]], splatting converts this into calling hcat([a1, a2, a3], [b1, b2, b3], [c1, c2, c3]).
+        mean_asp_rat_counts = mean(hcat([entry.asp_rat_counts for entry in matching_pressure_data]...), dims=2)
+        mean_asp_rat_bins = mean(hcat([entry.asp_rat_bins for entry in matching_pressure_data]...), dims=2)
+        mean_rot_ang_counts = mean(hcat([entry.rot_ang_counts for entry in matching_pressure_data]...), dims=2)
+        mean_rot_ang_bins = mean(hcat([entry.rot_ang_bins for entry in matching_pressure_data]...), dims=2)
+
+        @bp
+        
+        # Calculate bin widths
+        asp_rat_bin_widths = diff(mean_asp_rat_bins, dims=1)
+        rot_ang_bin_widths = diff(mean_rot_ang_bins, dims=1)
+
+        # Normalize counts to get probabilities, considering the bin widths
+        total_asp_count = sum(mean_asp_rat_counts .* asp_rat_bin_widths)
+        total_rot_count = sum(mean_rot_ang_counts .* rot_ang_bin_widths)
+        
+        probabilities_asp = (mean_asp_rat_counts ./ total_asp_count) ./ asp_rat_bin_widths
+        probabilities_rot = (mean_rot_ang_counts ./ total_rot_count) ./ rot_ang_bin_widths
+        plot_bins_asp = mean_asp_rat_bins[1:end-1]
+        plot_bins_rot = mean_rot_ang_bins[1:end-1]
+
+        # This is needed because MATLAB.jl has a hard time escaping \'s
+        pressure_label = @sprintf("\$ \\hat{P} = %.3f, \\hat{\\gamma} = %.3f , \\hat{\\omega} = %.3f \$", pressure_value, γ_value, ω_value)
+
+        mat"""
+        mean_asp_rat_counts = $(probabilities_asp);
+        mean_asp_rat_bins = $(plot_bins_asp);
+        mean_rot_ang_counts = $(probabilities_rot);
+        mean_rot_ang_bins = $(plot_bins_rot);
+        marker_color = $(marker_color);
+        pressure_label = $(pressure_label);
+        
+        figure(figure_aspect_ratio);
+        plot(mean_asp_rat_bins, mean_asp_rat_counts, '-o','MarkerFaceColor', marker_color, 'Color', marker_color, 'DisplayName', pressure_label);
+
+        figure(figure_rotation_angle);
+        plot(mean_rot_ang_bins, mean_rot_ang_counts, '-o','MarkerFaceColor', marker_color, 'Color', marker_color, 'DisplayName', pressure_label);
+        """
+    end
+    mat"""
+    figure(figure_aspect_ratio);
+    legend('show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
+
+    figure(figure_rotation_angle);
+    legend('show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
     """
 end
 
