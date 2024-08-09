7da767b1fb17daabc21056095b1f3de9c51d5709
Colton Kawamura <colton.kawamura1@nps.edu>
2024-08-09 06:46:23 -0400

    new job out

--- src/GranMA.jl
+++ src/GranMA.jl
@@ -51,7 +51,7 @@ end
 
 function generate_simulation_jobs(filename::String, K_values::Vector{T1}, M_values::Vector{T2}, Bv_values::Vector{T3}, w_D_values::Vector{T4}, N_values::Vector{T5}, P_values::Vector{T6}, W_values::Vector{T7}, seeds::Vector{T8}) where {T1, T2, T3, T4, T5, T6, T7, T8}
     # generate_simulation_jobs("testfile.txt", [100],[1],exp10.(-4:.5:1),exp10.(-4:.5:1),[10000],[.1],[10,20,50,100],[1,2,3,4,5])
-    # generate_simulation_jobs("simulation_job_list.txt", [100],[1],exp10.(-3:.5:1),exp10.(-3:.5:1),[5000],[0.0001,0.00021544, 0.00046416, 0.001, 0.0021544, 0.0046416, 0.01, 0.021544, 0.046416, 0.1, ],[5],[1,2,3,4,5])
+    # generate_simulation_jobs("simulation_job_list.txt", [100],[1],exp10.(-3:.1:1.5),exp10.(-3:.1:1.5),[5000],[0.0001,0.00021544, 0.00046416, 0.001, 0.0021544, 0.0046416, 0.01, 0.021544, 0.046416, 0.1, ],[5],[1,2,3,4,5])
     # Convert input vectors to the required types
     K_values = Int64.(K_values)
     M_values = Int64.(M_values)
