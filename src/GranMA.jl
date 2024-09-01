8ef9b9502d80c48e566be7ad43839a0913d9534e
Colton Kawamura <colton.kawamura1@nps.edu>
2024-09-01 06:12:33 -0400

    added file extension

--- src/GranMA.jl
+++ src/GranMA.jl
@@ -14,7 +14,7 @@ using JLD2
 using IterTools
 using Distributed
 
-export  generate_simulation_jobs,
+export  makeJobList2D,
         load_data,
         file_data,
         crunch_and_save,
@@ -68,9 +68,9 @@ mutable struct file_data
     timeVec::Vector{Float64}
 end
 
-function generate_simulation_jobs(filename::String, K_values::Vector{T1}, M_values::Vector{T2}, Bv_values::Vector{T3}, w_D_values::Vector{T4}, N_values::Vector{T5}, P_values::Vector{T6}, W_values::Vector{T7}, seeds::Vector{T8}) where {T1, T2, T3, T4, T5, T6, T7, T8}
-    # generate_simulation_jobs("testfile.txt", [100],[1],exp10.(-5:.2:2),exp10.(-5:.2:2),[10000],[.01,.001],[10,20,50],[1,2,3,4,5])
-    # generate_simulation_jobs("simulation_job_list.txt", [100],[1],exp10.(-5:.2:2),exp10.(-5:.2:2),[5000],[0.0001,0.00021544, 0.00046416, 0.001, 0.0021544, 0.0046416, 0.01, 0.021544, 0.046416, 0.1, ],[5],[1,2,3,4,5])    # Convert input vectors to the required types
+function makeJobList2D(filename::String, K_values::Vector{T1}, M_values::Vector{T2}, Bv_values::Vector{T3}, w_D_values::Vector{T4}, N_values::Vector{T5}, P_values::Vector{T6}, W_values::Vector{T7}, seeds::Vector{T8}) where {T1, T2, T3, T4, T5, T6, T7, T8}
+    # makeJobList2D("testfile.txt", [100],[1],exp10.(-5:.2:2),exp10.(-5:.2:2),[10000],[.01,.001],[10,20,50],[1,2,3,4,5])
+    # makeJobList2D("simulation_job_list.txt", [100],[1],exp10.(-5:.2:2),exp10.(-5:.2:2),[5000],[0.0001,0.00021544, 0.00046416, 0.001, 0.0021544, 0.0046416, 0.01, 0.021544, 0.046416, 0.1, ],[5],[1,2,3,4,5])    # Convert input vectors to the required types
     K_values = Int64.(K_values)
     M_values = Int64.(M_values)
     Bv_values = Float64.(Bv_values)
@@ -280,9 +280,6 @@ function load_data(filepath::String)::Vector{file_data}
     @load filepath simulation_data
     return simulation_data
 end
-
-
-
 
 function pack_poly_2d(N, K, D, G, M, P_target, W_factor, seed, plotit)
     mat"""
