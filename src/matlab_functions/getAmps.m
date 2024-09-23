72ec79b12d26ee7d42a5c5a02e9479dece9ce9a4
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-09-23 15:22:40 -0400

    made mroe adjustmets

--- src/matlab_functions/getAmps.m
+++ src/matlab_functions/getAmps.m
@@ -1,15 +1,25 @@
-function [breakOut, outAmp, outXinit] = getAmps(nt, x, x0, idx, w_D, A, outAmp, outXinit)
+function [breakOut, outAmp, outXinit] = getAmps(nt, x, x0, idx, w_D, A, outAmp, outXinit, Lx)
     breakOut = false;
     if mod(nt,10)==0
 
         smoothAmp = smooth(x(idx) - x0(idx), 150, 'sgolay');
-        minPeakWidth = 1/w_D * .6;
+        minPeakWidth = 1/w_D * .6; % Criteria is at a little more that a half wavelength, some buffer for weird stuff happening
         [pks,locs]=findpeaks(smoothAmp, "MinPeakWidth", minPeakWidth);
         valid_peaks_idx = find(pks > A * 0.1);
         if ~isempty(valid_peaks_idx) && length(valid_peaks_idx) > 2
             second_peak_idx = valid_peaks_idx(end-2); % this is the third peak
             second_peak_Amp = pks(second_peak_idx);
+
             second_peak_xInit = x0(idx(locs(second_peak_idx)));
+            if length(outXinit) > 1 && second_peak_xInit < outXinit(end-1) 
+                return
+            end
+
+            if second_peak_xInit > .8 * Lx 
+                return
+            end
+
+
             % plot(x0(idx), x(idx) - x0(idx), '.', x0(idx), smoothAmp, 'r-', x0(idx(peak_index)), x(idx(peak_index)) - x0(idx(peak_index)), 'o', 'MarkerFaceColor', 'r')
             plot(x0(idx), x(idx) - x0(idx), '.', x0(idx), smoothAmp, 'r-', second_peak_xInit, second_peak_Amp, 'o', 'MarkerFaceColor', 'r')
             ylim(1.2*[-A,A])
