400cc85120a130628d6d5a66c6adf0e2f3221820
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-10-09 11:06:36 -0400

    migrated wavespeed plot to plotting.jl

--- sandbox.jl
+++ sandbox.jl
@@ -843,99 +843,100 @@ function plot_γ_attenuation_P_2d(pressure_value, ω_values::Vector{Float64}; pl
     end
 end
 
-function plot_ωγ_wavespeed_2d(gamma_value) # Need to fix lgend
+# migrated to plotting.jl
+# function plot_ωγ_wavespeed_2d(gamma_value) # Need to fix lgend
 
-    # filter the data based on those that are close to gamma_value
-    closest_gamma_index = argmin(abs.([idx.gamma for idx in simulation_data] .- gamma_value))
-    closest_gamma_value = simulation_data[closest_gamma_index].gamma
-    matching_gamma_data = filter(entry -> entry.gamma == closest_gamma_value, simulation_data)
-    plot_gamma = gamma_value
+#     # filter the data based on those that are close to gamma_value
+#     closest_gamma_index = argmin(abs.([idx.gamma for idx in simulation_data] .- gamma_value))
+#     closest_gamma_value = simulation_data[closest_gamma_index].gamma
+#     matching_gamma_data = filter(entry -> entry.gamma == closest_gamma_value, simulation_data)
+#     plot_gamma = gamma_value
 
-    # Get a list of unique input pressures
-    pressure_list = unique([entry.pressure for entry in matching_gamma_data]) # goes through each entry of simulation_data and get the P value at that entry
-    plot_pressure = pressure_list
+#     # Get a list of unique input pressures
+#     pressure_list = unique([entry.pressure for entry in matching_gamma_data]) # goes through each entry of simulation_data and get the P value at that entry
+#     plot_pressure = pressure_list
 
-    # Define the plot limits to match the 1D theory plot curves
-    theory_x = collect(3E-4:1E-5:3)
-    theory_y = 1 ./ sqrt(2) .* ((1 .+ theory_x.^2) .* (1 .+ sqrt.(1 .+ theory_x.^2))).^(0.5) ./ (1 .+ theory_x.^2);
-    # upper_limit_line_x = [1*gamma_value; 1*gamma_value]
-    # upper_limit_line_y = [1E-5; 1]
-    # lower_limit_line_x = [.1*gamma_value; .1*gamma_value]
-    # lower_limit_line_y = [1E-5; 1]
-    # % plot($(upper_limit_line_x), $(upper_limit_line_y), 'k', 'DisplayName', '\$ \\omega_0 \$')
-    # % plot($(lower_limit_line_x), $(lower_limit_line_y), 'b', 'DisplayName', '\$ .1 \\omega_0 \$')
+#     # Define the plot limits to match the 1D theory plot curves
+#     theory_x = collect(3E-4:1E-5:3)
+#     theory_y = 1 ./ sqrt(2) .* ((1 .+ theory_x.^2) .* (1 .+ sqrt.(1 .+ theory_x.^2))).^(0.5) ./ (1 .+ theory_x.^2);
+#     # upper_limit_line_x = [1*gamma_value; 1*gamma_value]
+#     # upper_limit_line_y = [1E-5; 1]
+#     # lower_limit_line_x = [.1*gamma_value; .1*gamma_value]
+#     # lower_limit_line_y = [1E-5; 1]
+#     # % plot($(upper_limit_line_x), $(upper_limit_line_y), 'k', 'DisplayName', '\$ \\omega_0 \$')
+#     # % plot($(lower_limit_line_x), $(lower_limit_line_y), 'b', 'DisplayName', '\$ .1 \\omega_0 \$')
 
-    # Intialized the plots to loop over
-    mat"""
-    figure_wavespeed = figure;
-    loglog($(theory_x), 1./$(theory_y), 'k', 'DisplayName', '1-D Theory'), hold on
-    xlabel('\$\\hat{\\omega}\\hat{\\gamma}\$', "FontSize", 20, "Interpreter", "latex");
-    ylabel('\$\\hat{c} \$', "FontSize", 20, "Interpreter", "latex");
-    set(gca, 'XScale', 'log');
-    set(get(gca, 'ylabel'), 'rotation', 0);
-    grid on;
-    box on;
-    """
+#     # Intialized the plots to loop over
+#     mat"""
+#     figure_wavespeed = figure;
+#     loglog($(theory_x), 1./$(theory_y), 'k', 'DisplayName', '1-D Theory'), hold on
+#     xlabel('\$\\hat{\\omega}\\hat{\\gamma}\$', "FontSize", 20, "Interpreter", "latex");
+#     ylabel('\$\\hat{c} \$', "FontSize", 20, "Interpreter", "latex");
+#     set(gca, 'XScale', 'log');
+#     set(get(gca, 'ylabel'), 'rotation', 0);
+#     grid on;
+#     box on;
+#     """
 
-    # Normalize the gamma values
-    normalized_variable = (log.(pressure_list) .- minimum(log.(pressure_list))) ./ (maximum(log.(pressure_list)) .- minimum(log.(pressure_list)))
+#     # Normalize the gamma values
+#     normalized_variable = (log.(pressure_list) .- minimum(log.(pressure_list))) ./ (maximum(log.(pressure_list)) .- minimum(log.(pressure_list)))
 
-    # Create a line for each gamma value across all pressure_list
-    for pressure_value in pressure_list
+#     # Create a line for each gamma value across all pressure_list
+#     for pressure_value in pressure_list
 
-        # Assign a color
-        idx = findfirst(idx -> idx ==pressure_value, pressure_list) # find the first index that matches
-        marker_color = [normalized_variable[idx], 0, 1-normalized_variable[idx]]
+#         # Assign a color
+#         idx = findfirst(idx -> idx ==pressure_value, pressure_list) # find the first index that matches
+#         marker_color = [normalized_variable[idx], 0, 1-normalized_variable[idx]]
 
-        # Only look at data for current pressure value
-        matching_pressure_data = filter(entry -> entry.pressure == pressure_value, matching_gamma_data) # for every entry in simluation_data, replace (->) that entry with result of the boolean expression
+#         # Only look at data for current pressure value
+#         matching_pressure_data = filter(entry -> entry.pressure == pressure_value, matching_gamma_data) # for every entry in simluation_data, replace (->) that entry with result of the boolean expression
 
-        # Initizalized vectors for just this pressure
-        loop_mean_wavespeed_list = Float64[];
+#         # Initizalized vectors for just this pressure
+#         loop_mean_wavespeed_list = Float64[];
 
-        # Look at a single omega gamma value since each one spans all seeds
-        matching_omega_gamma_list = sort(unique([entry.omega_gamma for entry in matching_pressure_data]))
+#         # Look at a single omega gamma value since each one spans all seeds
+#         matching_omega_gamma_list = sort(unique([entry.omega_gamma for entry in matching_pressure_data]))
 
-        for omega_gamma_value in matching_omega_gamma_list
+#         for omega_gamma_value in matching_omega_gamma_list
 
-            # Only look at data for current pressure value
-            matching_omega_gamma_data = filter(entry -> entry.omega_gamma == omega_gamma_value, matching_pressure_data) # for every entry in simluation_data, replace (->) that entry with result of the boolean expression
-            @bp
-            # Get the mean over all seeds
-            loop_mean_wavespeed = mean(entry.:wavespeed_x for entry in matching_omega_gamma_data)
-            @bp
-            # Append values
-            push!(loop_mean_wavespeed_list, loop_mean_wavespeed)
-        end
+#             # Only look at data for current pressure value
+#             matching_omega_gamma_data = filter(entry -> entry.omega_gamma == omega_gamma_value, matching_pressure_data) # for every entry in simluation_data, replace (->) that entry with result of the boolean expression
+#             @bp
+#             # Get the mean over all seeds
+#             loop_mean_wavespeed = mean(entry.:wavespeed_x for entry in matching_omega_gamma_data)
+#             @bp
+#             # Append values
+#             push!(loop_mean_wavespeed_list, loop_mean_wavespeed)
+#         end
 
-        # Filter data to include only points where omega_gamma <= gamma_value
-        valid_indices = matching_omega_gamma_list .<= gamma_value.*2
-        matching_omega_gamma_list = matching_omega_gamma_list[valid_indices]
-        loop_mean_wavespeed_list = loop_mean_wavespeed_list[valid_indices]
+#         # Filter data to include only points where omega_gamma <= gamma_value
+#         valid_indices = matching_omega_gamma_list .<= gamma_value.*2
+#         matching_omega_gamma_list = matching_omega_gamma_list[valid_indices]
+#         loop_mean_wavespeed_list = loop_mean_wavespeed_list[valid_indices]
         
-        mat"""
-        omega_gamma = $(matching_omega_gamma_list);
-        mean_wavespeed_x = $(loop_mean_wavespeed_list);
-        iloop_pressure_value = $(pressure_value);
-        plot_gamma = $(plot_gamma);
-        marker_color = $(marker_color);
-        pressure_label = sprintf('Wavespeed X = %.2f, Gamma = %.2f (Aspect Ratio)', $(pressure_value), $(plot_gamma));
-        % pressure_label = "\$ \\alpha x^2 \$ = iloop_pressure_value, \\gamma = plot_gamma \\mathrm{(Attenuation)}"
-        pressure_label2 = sprintf('Pressure = %.2f, Gamma = %.2f (Wavespeed X)', $(pressure_value), $(plot_gamma));
+#         mat"""
+#         omega_gamma = $(matching_omega_gamma_list);
+#         mean_wavespeed_x = $(loop_mean_wavespeed_list);
+#         iloop_pressure_value = $(pressure_value);
+#         plot_gamma = $(plot_gamma);
+#         marker_color = $(marker_color);
+#         pressure_label = sprintf('Wavespeed X = %.2f, Gamma = %.2f (Aspect Ratio)', $(pressure_value), $(plot_gamma));
+#         % pressure_label = "\$ \\alpha x^2 \$ = iloop_pressure_value, \\gamma = plot_gamma \\mathrm{(Attenuation)}"
+#         pressure_label2 = sprintf('Pressure = %.2f, Gamma = %.2f (Wavespeed X)', $(pressure_value), $(plot_gamma));
         
-        figure(figure_wavespeed);
-        set(gca, 'Yscale', 'log');
-        plot(omega_gamma, mean_wavespeed_x, '-o','MarkerFaceColor', marker_color, 'Color', marker_color, 'DisplayName', pressure_label2);
-        """
-    end
+#         figure(figure_wavespeed);
+#         set(gca, 'Yscale', 'log');
+#         plot(omega_gamma, mean_wavespeed_x, '-o','MarkerFaceColor', marker_color, 'Color', marker_color, 'DisplayName', pressure_label2);
+#         """
+#     end
 
-    # Add legends to the plots
-    mat"""
-    % Add legends to the MATLAB plots
-    figure(figure_wavespeed);
-    legend('show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
-    """
-end
+#     # Add legends to the plots
+#     mat"""
+#     % Add legends to the MATLAB plots
+#     figure(figure_wavespeed);
+#     legend('show', 'Location', 'northeastoutside', 'Interpreter', 'latex');
+#     """
+# end
 
 
 function plot_attenuation_width_effect(γ_value, pressure_value)
