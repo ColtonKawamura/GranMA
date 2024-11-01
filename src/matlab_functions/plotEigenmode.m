40d10343c593c3a7bf2ebee405cc26daba88e34e
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-11-01 11:52:54 -0400

    made quiver plot

--- src/matlab_functions/plotEigenmode.m
+++ src/matlab_functions/plotEigenmode.m
@@ -12,14 +12,13 @@ function plotEigenmode(x0, y0, eigen_vectors, mode_to_plot)
     
     % Populate dx and dy for each particle based on eigen_vectors
     for i = 1:N
-        dx(i) = mode_positions(2*i-1) - x0(i); % x displacement
-        dy(i) = mode_positions(2*i) - y0(i);   % y displacement
+        dx(i) = mode_positions(2*i-1) - x0(i); % Particle  x-position are odd rows 2*i-1 = 1,3,4...
+        dy(i) = mode_positions(2*i) - y0(i);   % y-positions are even rows 2*i = 2,4,6...
     end
     
-    quiver(x0, y0, dx, dy, 0, 'AutoScale', 'off');
-    title(['Quiver plot for eigenmode ', num2str(mode_to_plot)]);
-    xlabel('X Position');
-    ylabel('Y Position');
+    quiver(x0, y0, dx, dy)
+    title(['Eigenmode ', num2str(mode_to_plot)]);
     axis equal;
     grid on;
+    % ylim([0,y_limit])
 end
