8f9ffd57b2b35add3bc868210fbfd4e1532288a4
Colton Kawamura <colton.kawamura1@nps.edu>
2024-11-08 09:43:43 -0500

    Allowed to automatrical pull the right packing based on value of N

--- src/matlab_functions/eigenTest.m
+++ src/matlab_functions/eigenTest.m
@@ -1,13 +1,13 @@
 % This is a script not a function.
 
 %  Make the packing
-N = 20;
+N = 30;
 Dn_list = 1+0.4*(rand([1,N])>0.5); 
 packper103f(1,1,Dn_list,0,1);
 
 %  Get eigenstuff
-
-load("in/2D_Repeating/2D_N20_P0_Width0_Seed1.mat")
+file_name = sprintf('in/2D_Repeating/2D_N%d_P0_Width0_Seed1.mat', N);
+load(file_name)
 
 [Hessian, eigen_values, eigen_vectors] = HessYale(x, y, Dn, N, Ly);
 
@@ -15,3 +15,5 @@ load("in/2D_Repeating/2D_N20_P0_Width0_Seed1.mat")
 mode_to_plot = 3; % should just be the column
 figure;
 plotEigenmode(x', y', eigen_vectors, mode_to_plot)
+
+eigen_values
