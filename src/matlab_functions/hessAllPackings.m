a80c4a812148e758f80693acd53ec19cf0b100ec
Colton Kawamura <colton.kawamura1@nps.edu>
2025-01-07 07:54:18 -0500

    changed it to file name list

--- src/matlab_functions/hessAllPackings.m
+++ src/matlab_functions/hessAllPackings.m
@@ -1,43 +1,51 @@
-function hessAllPackings(path_to_packings, path_to_store)
+function hessAllPackings(file_name_list, path_to_store)
     % hessAllPackings computes the eigenvectors and eigenvalues of the Hessian 
-    % matrix for each packing in the specified directory and saves them in a 
+    % matrix for each packing in the specified list of files and saves them in a 
     % separate file.
     % 
     % Inputs:
-    %   path_to_packings: Path where the input *.mat files are located.
+    %   file_name_list: A cell array or string array of full paths to the *.mat files.
     %   path_to_store: Path where the eigenvectors and eigenvalues should be saved.
 
-    % Get list of all *.mat files in the specified directory
-    file_list = dir(fullfile(path_to_packings, '*.mat'));
-    
     % Loop through each file in the list
-    for i = 1:length(file_list)
-        % Construct the full file name
-        file_name = fullfile(path_to_packings, file_list(i).name);
+    for i = 1:length(file_name_list)
+        % Get the current file name
+        file_name = file_name_list{i};
         
-        load(file_name, 'x', 'y', 'Dn', 'K', 'Ly', 'Lx');
-        
-        % Prepare positions and radii
-        positions = [x', y'];
-        radii = Dn ./ 2;
-        
-        % Clean the data using the cleanRats function (you need to define cleanRats)
-        [positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
-        
-        % Compute the Hessian matrix using hess2d (you need to define hess2d)
-        Hessian = hess2d(positions, radii, K, Ly, Lx);
-        
-        % Calculate the eigenvalues and eigenvectors of the Hessian
-        [eigen_vectors, eigen_values] = eig(Hessian);
-        
-        % Define the output filename (using the original file name without the .mat extension)
-        [~, filename, ~] = fileparts(file_name); % Get the base file name
-        output_file_name = fullfile(path_to_store, [filename, '_eig.mat']);
-        
-        % Save the eigenvectors and eigenvalues in the specified location
-        save(output_file_name, 'eigen_vectors', 'eigen_values');
-        clear x y Dn K Ly Lx positions radii Hessian eigen_vectors eigen_values; % If I get an OOM fault I'm going to throw a chair
-        
-        fprintf('Processed file: %s\n', file_name);
+        try
+            % Try to load the required variables from the .mat file
+            load(file_name, 'x', 'y', 'Dn', 'K', 'Ly', 'Lx');
+            
+            % Prepare positions and radii
+            positions = [x', y'];
+            radii = Dn ./ 2;
+            
+            % Clean the data using the cleanRats function (you need to define cleanRats)
+            [positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+            
+            % Compute the Hessian matrix using hess2d (you need to define hess2d)
+            Hessian = hess2d(positions, radii, K, Ly, Lx);
+            
+            % Calculate the eigenvalues and eigenvectors of the Hessian
+            [eigen_vectors, eigen_values] = eig(Hessian);
+            
+            % Define the output filename (using the original file name without the .mat extension)
+            [~, filename, ~] = fileparts(file_name); % Get the base file name
+            output_file_name = fullfile(path_to_store, ['hess_', filename, '_eig.mat']); % Add "hess_" prefix
+            
+            % Save the eigenvectors and eigenvalues in the specified location using MAT-file version 7.3
+            save(output_file_name, 'eigen_vectors', 'eigen_values', '-v7.3');
+            
+            % Clear all variables to free memory after processing this file
+            clear x y Dn K Ly Lx positions radii Hessian eigen_vectors eigen_values;
+            
+            % Display progress
+            fprintf('Processed file: %s\n', file_name);
+            
+        catch ME
+            % If an error occurs (e.g., missing variables), print a warning and continue
+            fprintf('Error processing file: %s. Skipping this file.\n', file_name);
+            fprintf('Error message: %s\n', ME.message);
+        end
     end
 end
