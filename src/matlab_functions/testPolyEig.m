423b14ce630c2b8986ee91042ef852cc25a66a94
Colton Kawamura <colton.kawamura1@nps.edu>
2025-01-09 12:39:10 -0500

    initial commit

---
+++ src/matlab_functions/testPolyEig.m
@@ -0,0 +1,42 @@
+% Define the system parameters
+m = 1;  % Mass matrix (m = 1 for simplicity)
+k = 10;  % Spring constant (stiffness matrix)
+beta = 0.05;  % Damping 
+N = 3; % number of particles
+
+% Define the matrices K, D, and M
+K = k * eye(N);  % Stiffness matrix
+% make the off diagnal terms -k
+for i = 1:N-1
+    K(i, i+1) = -k;
+    K(i+1, i) = -k;
+end
+% K(1, end) = -k;
+% K(end, 1) = -k
+D = beta * (eye(N) + 0.1 * (rand(N) - 0.5)); % adding some off-diagnal terms; creates eye matrix, then adds a small randome number to each element. Thens hifts them down by 0.5 to make sure they are positive
+M = m * eye(N);  % Mass matrix
+
+
+D = (D + D') / 2;  % Make D symmetric
+% System matrix (K + i*omega*beta*D - omega^2*M) 
+% system_matrix = K + 1i * beta * D - M;  %
+
+% Solve for the eigenvalues using the polyeig function
+% eigenvalues = polyeig(system_matrix)
+
+K
+D
+M
+
+% Try this way
+[eigenvectors, eigenvalues] = polyeig(K, D, -M)
+
+for ii = 1:length(eigenvalues)
+    figure;
+    plot(real(eigenvectors(:, ii)), '-o');
+    title(['Eigenmode ' num2str(ii)]);
+    xlabel('particle position');
+    ylabel('Mode displacement');
+    grid on;
+    % pause; 
+end
