ad9e29e447f79280cdf1b96a9b70d55af37928a4
Colton Kawamura <colton.kawamura1@nps.edu>
2025-01-15 08:03:38 -0800

    feat: add plot_damped_freqs function for visualizing damping effects on frequencies

---
+++ src/matlab_functions/plot_damped_freqs.m
@@ -0,0 +1,212 @@
+load("in/damped_eig_test/2D_N14_P0.1_Width3_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.1;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass)
+
+
+% High Damping
+load("in/damped_eig_test/2D_N100_P0.1_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.5;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass); 
+[eigen_vectors_high, eigen_values_high] = polyeig(matSpring, matDamp, matMass);
+
+load("in/damped_eig_test/2D_N100_P0.01_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.5;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+[eigen_vectors_mid, eigen_values_mid] = polyeig(matSpring, matDamp, matMass);
+
+load("in/damped_eig_test/2D_N100_P0.001_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.5;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+[eigen_vectors_low, eigen_values_low] = polyeig(matSpring, matDamp, matMass);
+
+figure
+scatter( abs(imag(eigen_values_high)), -real(eigen_values_high), 'r', "DisplayName", 'P=0.1')
+hold on
+scatter( abs(imag(eigen_values_mid)), -real(eigen_values_mid), 'g', "DisplayName", 'P=0.01')
+scatter( abs(imag(eigen_values_low)), -real(eigen_values_low), 'b', "DisplayName", 'P=0.001')
+xlabel('Frequency')
+ylabel('Damping')
+grid on
+title('High Damping')
+
+% Medium Damping
+load("in/damped_eig_test/2D_N100_P0.1_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.05;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass)
+[eigen_vectors_high, eigen_values_high] = polyeig(matSpring, matDamp, matMass)
+
+load("in/damped_eig_test/2D_N100_P0.01_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.05;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass)
+[eigen_vectors_mid, eigen_values_mid] = polyeig(matSpring, matDamp, matMass)
+
+load("in/damped_eig_test/2D_N100_P0.001_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.05;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+[eigen_vectors_low, eigen_values_low] = polyeig(matSpring, matDamp, matMass);
+
+figure
+scatter( abs(imag(eigen_values_high)), -real(eigen_values_high), 'r', "DisplayName", 'P=0.1')
+hold on
+scatter( abs(imag(eigen_values_mid)), -real(eigen_values_mid), 'g', "DisplayName", 'P=0.01')
+scatter( abs(imag(eigen_values_low)), -real(eigen_values_low), 'b', "DisplayName", 'P=0.001')
+xlabel('Frequency')
+ylabel('Damping')
+grid on
+title('Medium Damping')
+
+% Low Damping
+load("in/damped_eig_test/2D_N100_P0.1_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.005;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+[eigen_vectors_high, eigen_values_high] = polyeig(matSpring, matDamp, matMass);
+
+load("in/damped_eig_test/2D_N100_P0.01_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.005;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+[eigen_vectors_mid, eigen_values_mid] = polyeig(matSpring, matDamp, matMass);
+
+load("in/damped_eig_test/2D_N100_P0.001_Width10_Seed1.mat")
+positions = [x',y'];
+radii = Dn./2;
+[positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+mass = 1;
+damping_constant = 0.005;
+[matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+[eigen_vectors_low, eigen_values_low] = polyeig(matSpring, matDamp, matMass);
+
+figure
+scatter( abs(imag(eigen_values_high)), -real(eigen_values_high), 'r', "DisplayName", 'P=0.1')
+hold on
+scatter( abs(imag(eigen_values_mid)), -real(eigen_values_mid), 'g', "DisplayName", 'P=0.01')
+scatter( abs(imag(eigen_values_low)), -real(eigen_values_low), 'b', "DisplayName", 'P=0.001')
+xlabel('Frequency')
+ylabel('Damping')
+grid on
+title('Low Damping')
+
+
+% Automated 
+damping_constants = [0.2, 0.05, 0.005];
+titles = {'High Damping', 'Medium Damping', 'Low Damping'};
+
+figure
+hold on
+
+for i = 1:length(damping_constants)
+        damping_constant = damping_constants(i);
+        
+        % High Damping
+        load("in/damped_eig_test/2D_N100_P0.1_Width10_Seed1.mat")
+        positions = [x', y'];
+        radii = Dn ./ 2;
+        [positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+        mass = 1;
+        [matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+        [eigen_vectors_high, eigen_values_high] = polyeig(matSpring, matDamp, matMass);
+
+        load("in/damped_eig_test/2D_N100_P0.01_Width10_Seed1.mat")
+        positions = [x', y'];
+        radii = Dn ./ 2;
+        [positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+        mass = 1;
+        [matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+        [eigen_vectors_mid, eigen_values_mid] = polyeig(matSpring, matDamp, matMass);
+
+        load("in/damped_eig_test/2D_N100_P0.001_Width10_Seed1.mat")
+        positions = [x', y'];
+        radii = Dn ./ 2;
+        [positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+        mass = 1;
+        [matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+        [eigen_vectors_low, eigen_values_low] = polyeig(matSpring, matDamp, matMass);
+
+        scatter(abs(imag(eigen_values_high)), -real(eigen_values_high), damping_constant*100, 'r', "DisplayName", ['P=0.1, ' titles{i}])
+        scatter(abs(imag(eigen_values_mid)), -real(eigen_values_mid), damping_constant*100, 'g', "DisplayName", ['P=0.01, ' titles{i}])
+        scatter(abs(imag(eigen_values_low)), -real(eigen_values_low), damping_constant*100, 'b', "DisplayName", ['P=0.001, ' titles{i}])
+end
+
+xlabel('Frequency')
+ylabel('Damping')
+grid on
+legend
+title('Damping vs Frequency for Different Damping Constants')
+hold off
+
+
+% Automated 
+damping_constants = [0.2, 0.05, 0.005];
+titles = {'High Damping', 'Medium Damping', 'Low Damping'};
+
+figure
+hold on
+
+% pressures = ["0.1", "0.01", "0.001"];
+pressures = ["0.001"];
+damping_constants = [0.2, 0.05, 0.005];
+titles = {'High Damping', 'Medium Damping', 'Low Damping'};
+
+figure
+hold on
+
+for i = 1:length(damping_constants)
+    damping_constant = damping_constants(i);
+    
+    for j = 1:length(pressures)
+        pressure = pressures(j);
+        
+        filename = sprintf("in/damped_eig_test/2D_N100_P%s_Width10_Seed1.mat", pressure);
+        load(filename)
+        positions = [x', y'];
+        radii = Dn ./ 2;
+        [positions, radii] = cleanRats(positions, radii, K, Ly, Lx);
+        mass = 1;
+        [matSpring, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, damping_constant, mass);
+        [eigen_vectors, eigen_values] = polyeig(matSpring, matDamp, matMass);
+        
+        scatter(abs(imag(eigen_values)), -real(eigen_values), damping_constant*100, 'DisplayName', sprintf('P=%.3f, %s', pressure, titles{i}))
+        end
+    end
+
+    xlabel('Frequency')
+    ylabel('Damping')
+    grid on
+    legend
+    title('Damping vs Frequency for Different Damping Constants and Pressures')
+    hold off
