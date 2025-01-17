a61dc38c4d250087c73f7d51bb5c24ec9636481a
Colton Kawamura <colton.kawamura1@nps.edu>
2025-01-17 12:20:39 -0800

    add: now can plot single pressure with many gamma

--- src/matlab_functions/plotDampedModeDensityPDF.m
+++ src/matlab_functions/plotDampedModeDensityPDF.m
@@ -11,31 +11,34 @@ function plotDampedModeDensityPDF(struct_file, pressure_list, damping_list)
 % More note: this assumes that the eigen_values = damping + i frequency
 % plotDampedModeDensityPDF("out/2d_damped_eigenStuff/2D_damped_eigenstuff_N400_K100_M1.mat", [.2, .01, .001], [.1])
 load(struct_file)
-damping_constant = damping_list(1); % This is temporary unless we need a reason to plot multiple damping on one plot
+% damping_constant = damping_list(1); % This is temporary unless we need a reason to plot multiple damping on one plot
 
 figure
 for i = 1:length(pressure_list)
     pressure = pressure_list(i);
 
-    % positions = [x',y'];  % Assuming x, y are in the loaded .mat file
-    % radii = Dn./2;  % Assuming Dn is in the loaded .mat file
+    for j = 1:length(damping_list)
+        % positions = [x',y'];  % Assuming x, y are in the loaded .mat file
+        % radii = Dn./2;  % Assuming Dn is in the loaded .mat file
+        damping_constant = damping_list(j);
+        eigen_values = findInStruct(results, {'pressure', 'damping'}, {pressure, damping_constant}, 'eigen_values'); 
 
-    eigen_values = findInStruct(results, {'pressure', 'damping'}, {pressure, damping_constant}, 'eigen_values'); 
-    eigen_values = eigen_values{1}; % need to do this because the struct saves it as an 1-deep array
+        eigen_values = eigen_values{1}; % need to do this because the struct saves it as an 1-deep array
 
-    [edges, normalized_counts] = modeDensity(abs(imag(eigen_values)));
-    [~, marker_color] = normVarColor(pressure_list, pressure, 1);
+        [edges, normalized_counts] = modeDensity(abs(imag(eigen_values)));
+        [~, marker_color] = normVarColor(pressure_list, pressure, 1);
 
-    % plot(edges, normalized_counts, '-o', 'MarkerFaceColor', marker_color, 'MarkerEdgeColor', marker_color, 'Color', marker_color, 'DisplayName', sprintf('$ P = %.3f $', pressure));
-    plot(edges, normalized_counts, '-o', 'MarkerFaceColor', marker_color, 'MarkerEdgeColor', marker_color, 'Color', marker_color);
-    xlabel('eigen frequencies (edges)', 'Interpreter', 'latex', 'FontSize', 20)
-    ylabel('counts', 'Interpreter', 'latex', 'FontSize', 20)
-    % legend('show', 'Interpreter', 'latex');
-    % title(sprintf('$L_x$ by $L_y$: %.2f by %.2f', Lx, Ly), 'Interpreter', 'latex', 'FontSize', 16);
-    set(gca, "XScale", "log")
-    set(gca, "YScale", "log")
-    grid on; 
-    hold on;
+        % plot(edges, normalized_counts, '-o', 'MarkerFaceColor', marker_color, 'MarkerEdgeColor', marker_color, 'Color', marker_color, 'DisplayName', sprintf('$ P = %.3f $', pressure));
+        plot(edges, normalized_counts, '-o', 'MarkerFaceColor', marker_color, 'MarkerEdgeColor', marker_color, 'Color', marker_color);
+        xlabel('eigen frequencies (edges)', 'Interpreter', 'latex', 'FontSize', 20)
+        ylabel('counts', 'Interpreter', 'latex', 'FontSize', 20)
+        % legend('show', 'Interpreter', 'latex');
+        % title(sprintf('$L_x$ by $L_y$: %.2f by %.2f', Lx, Ly), 'Interpreter', 'latex', 'FontSize', 16);
+        set(gca, "XScale", "log")
+        set(gca, "YScale", "log")
+        grid on; 
+        hold on;
+    end
 end
 
 end
