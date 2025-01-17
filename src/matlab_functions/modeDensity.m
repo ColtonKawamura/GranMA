88b2fad74cf56cd6ed93b6d2e5a5f414a4e5c015
Colton Kawamura <colton.kawamura1@nps.edu>
2025-01-17 12:18:07 -0800

    change: decreased bin size from 100 to 30 for damped visualization (not many modes)

--- src/matlab_functions/modeDensity.m
+++ src/matlab_functions/modeDensity.m
@@ -8,7 +8,7 @@ function [edges, normalized_counts] = modeDensity(eigen_values)
     % eigen_values_diag = diag(eigen_values);  % eigen values ar ethe diags
     sqrt_eigen_values = sqrt(eigen_values_diag);
     sorted_sqrt_eigen_values = sort(sqrt_eigen_values);
-    [counts, edges] = hist(sorted_sqrt_eigen_values./10, 100); % 100 bins, 10 is the natural frequency
+    [counts, edges] = hist(sorted_sqrt_eigen_values./10, 30); % 100 bins, 10 is the natural frequency
     norm = sum(counts)*(edges(2)-edges(1));
     normalized_counts = counts./norm;
     % figure; 
