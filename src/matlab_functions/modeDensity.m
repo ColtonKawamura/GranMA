aa3314fc216b0f9451f095af03dd5ea1882e0dd8
Colton Kawamura <colton.kawamura1@nps.edu>
2025-01-16 17:30:18 -0800

    add: feature to handle eigenvalues as a vector

--- src/matlab_functions/modeDensity.m
+++ src/matlab_functions/modeDensity.m
@@ -1,8 +1,14 @@
 function [edges, normalized_counts] = modeDensity(eigen_values)
-    eigen_values_diag = diag(eigen_values);  % eigen values ar ethe diags
+    [s1, s2] = size(eigen_values);
+    if s1 == 1 || s2 == 1
+        eigen_values_diag = eigen_values; % this is for the struct (damped flow)
+    else
+        eigen_values_diag = diag(eigen_values); % this is for the case where the eigen_values are a matrix
+    end
+    % eigen_values_diag = diag(eigen_values);  % eigen values ar ethe diags
     sqrt_eigen_values = sqrt(eigen_values_diag);
     sorted_sqrt_eigen_values = sort(sqrt_eigen_values);
-    [counts, edges] = hist(sorted_sqrt_eigen_values./10, 100);
+    [counts, edges] = hist(sorted_sqrt_eigen_values./10, 100); % 100 bins, 10 is the natural frequency
     norm = sum(counts)*(edges(2)-edges(1));
     normalized_counts = counts./norm;
     % figure; 
