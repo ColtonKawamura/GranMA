78c9dbd8afdfb439f5df297b8f3a7a28f15b3d53
Colton Kawamura <c.kawamura@me.com>
2025-02-26 08:47:10 -0500

    add: feature to flag shear plots

--- src/matlab_functions/visualizeSim.m
+++ src/matlab_functions/visualizeSim.m
@@ -1,8 +1,11 @@
-function visualizeSim(nt, x, x0, idx, A )
+function visualizeSim(nt, x, x0, idx, A, flag_shear)
     if mod(nt,1)==0
-
             plot(x0(idx), x(idx) - x0(idx),'o')
-            ylim(1.2*[-A,A])
+            if flag_shear == true
+                ylim(1.2*[-A*.1,A*.1])
+            else
+                ylim(1.2*[-A,A])
+            end
             xlabel('$x_0$', 'Interpreter', 'latex', 'FontSize', 20)
             ylabel('$ \Delta x$', 'Interpreter', 'latex', 'FontSize', 20)
             grid on
