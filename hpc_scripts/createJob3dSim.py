d1bc2cdd508aaf16d80c841ced77237cfda948df
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2025-03-18 20:53:29 -0400

    fix: corrected folder name

--- hpc_scripts/createJob3dSim.py
+++ hpc_scripts/createJob3dSim.py
@@ -10,14 +10,16 @@ def main():
     # Define different values for each variable
     K_values = [100]
     M_values = [1]
-    Bv_values = [.01, .05, .1, .5, 1, 5, 10,50, 100] 
-    w_D_values = [.01, .05, .1, .5, 1, 5, 10,50, 100]
+    # Bv_values = [.01, .05, .1, .5, 1, 5, 10,50, 100] 
+    Bv_values = np.linspace(0.01, 100, 20)
+    # w_D_values = [.01, .05, .1, .5, 1, 5, 10,50, 100]
+    w_D_values = np.linspace(0.01, 100, 20)
     N_values = [75600]
     P_values = [0.1, 0.01, 0.001]
     W_values = [15] # width of the simulation box
     seed_values = [1, 2, 3]
-    in_path = 'in/3d/tiled_15by300/'  # Example input path
-    out_path = 'out/simulation_3d/three_pressures_V2/'  # Example output path
+    in_path = 'in/3D/tiled_15by300/'  # Example input path
+    out_path = 'out/simulation_3d/three_pressures_V3/'  # Example output path
     output_file = "./matlab_commands.txt"
 
     with open(output_file, "w") as file:
