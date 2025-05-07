dd0b2154d5ebabef5dff4b93efde4dfa4ab7ef60
Colton Kawamura <c.kawamura@me.com>
2025-05-06 21:15:32 -0400

    added plot lines

--- src/matlab_functions/paperEigenStuff.m
+++ src/matlab_functions/paperEigenStuff.m
@@ -56,13 +56,12 @@ slopeLine('loglog' ,0, [.1,1], .4, 'TextLocation', [.4, .5])
 
 %% Undamped - Plot Eigen Vectors
 load("in/2d_tile_20by20/40by40/2D_N1600_P0.1_Width40_Seed1.mat"); 
-load("out/junkyard/2D_damped_eigenstuff_N1483_40by56_K100_M1.mat") % test - damped algo with undamped data
+% load("out/junkyard/2D_damped_eigenstuff_N1483_40by56_K100_M1.mat") % test - damped algo with undamped data
 % ---- Maybe point where needs own function ------
 positions = [x',y']; 
 radii = Dn/2;  
 Hessian = hess2d(positions, radii', K, Ly, Lx);
 [eigenVectors, eigenValues ] = eig(Hessian);
-% -------------------------------------
 modesToPlot = 1:10;
 for i = 1:length(modesToPlot)
     modeToPlot = modesToPlot(i);
@@ -71,65 +70,58 @@ for i = 1:length(modesToPlot)
 end
 
 
- % Gaussian
- sim2dGauss(100, 1, .0001, 1, 5000, 0.1, 10, 1, "in/2d_5wide_1000long/", "out/junk_yard")
-
-
-
 %  Small Packing for Debugging
 %  Process the small packing
 % processEigenModesDampedPara("in/2d_damped_eigen_small/", "out/junkyard/", [0])
-load("out/junkyard/2D_damped_eigenstuff_N14_5by4_K100_M1.mat", "outData"); % Small packing
-outData = orderPolyEig(outData);
-plotData = filterData(outData, 'pressure', .1, 'damping',0)
-% [~, idx] = sort(abs(imag(plotData.eigenValues{1})));
-% plotData.eigenVectors{1} = plotData.eigenVectors{1}(:, idx);
-x = plotData.positions{1}(:, 1);
-y = plotData.positions{1}(:, 2);
-eigenVectors = plotData.eigenVectors{1};
-modeToPlot = 1;
-plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', true);
+% load("out/junkyard/2D_damped_eigenstuff_N14_5by4_K100_M1.mat", "outData"); % Small packing
+% outData = orderPolyEig(outData);
+% plotData = filterData(outData, 'pressure', .1, 'damping',0)
+% % [~, idx] = sort(abs(imag(plotData.eigenValues{1})));
+% % plotData.eigenVectors{1} = plotData.eigenVectors{1}(:, idx);
+% x = plotData.positions{1}(:, 1);
+% y = plotData.positions{1}(:, 2);
+% eigenVectors = plotData.eigenVectors{1};
+% modeToPlot = 1;
+% plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', true);
 
-% This verifies that the spring matrix is correct
-% It does what processEigenModesDamped does when you do matSpringDampMass
-load("in/2d_damped_eigen_small/2D_N14_P0.1_Width3_Seed1.mat")
-positions = [x',y']; 
-radii = Dn'/2;  
-[Hessian, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, 0,1 );
-[eigenVectors, eigenValues] = polyeig(Hessian, matDamp, matMass);
-% [eigenVectors, eigenValues ] = eig(Hessian); % this plots correct
-% sort the eigenValues and eigenVectors
-[~, idx] = sort(abs(imag(eigenValues)));
-eigenVectors = eigenVectors(:, idx);
+% % This verifies that the spring matrix is correct
+% % It does what processEigenModesDamped does when you do matSpringDampMass
+% load("in/2d_damped_eigen_small/2D_N14_P0.1_Width3_Seed1.mat")
+% positions = [x',y']; 
+% radii = Dn'/2;  
+% [Hessian, matDamp, matMass] = matSpringDampMass(positions, radii, K, Ly, Lx, 0,1 );
+% [eigenVectors, eigenValues] = polyeig(Hessian, matDamp, matMass);
+% % [eigenVectors, eigenValues ] = eig(Hessian); % this plots correct
+% % sort the eigenValues and eigenVectors
+% [~, idx] = sort(abs(imag(eigenValues)));
+% eigenVectors = eigenVectors(:, idx);
 
-x = positions(:, 1);
-y = positions(:, 2);
-modeToPlot = 1;
-modesToPlot = 1:size(eigenVectors,2);
-for i = 1:length(modesToPlot)
-    modeToPlot = modesToPlot(i);
-    plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', true);
-    input('Press Enter to continue...');
-end
-plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', true);
+% x = positions(:, 1);
+% y = positions(:, 2);
+% modeToPlot = 1;
+% modesToPlot = 1:size(eigenVectors,2);
+% for i = 1:length(modesToPlot)
+%     modeToPlot = modesToPlot(i);
+%     plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', true);
+%     input('Press Enter to continue...');
+% end
+% plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', true);
 
-% Undamped
-load("in/2d_damped_eigen_small/2D_N14_P0.1_Width3_Seed1.mat")
-positions = [x',y']; 
-radii = Dn'/2;  
-Hessian = hess2d(positions, radii, K, Ly, Lx);
-[eigenVectors, eigenValues ] = eig(Hessian);
-x = positions(:, 1);
-y = positions(:, 2);
-modeToPlot = 1;
-plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', false);
+% % Undamped
+% load("in/2d_damped_eigen_small/2D_N14_P0.1_Width3_Seed1.mat")
+% positions = [x',y']; 
+% radii = Dn'/2;  
+% Hessian = hess2d(positions, radii, K, Ly, Lx);
+% [eigenVectors, eigenValues ] = eig(Hessian);
+% x = positions(:, 1);
+% y = positions(:, 2);
+% modeToPlot = 1;
+% plotEigenmode(x, y, eigenVectors, modeToPlot, 'damped', false);
 
 %% Damped Imaginay vs Real Eigenvalues
 load("out/2d_damped_eigenStuff/2D_damped_eigenstuff_N1483_40by56_K100_M1.mat", "outData"); 
 outData = orderPolyEig(outData);
 plotData = filterData(outData, 'pressure', .1, 'damping', .001)
-x = plotData.positions{1}(:, 1);
-y = plotData.positions{1}(:, 2);
 realEigenValues = real(plotData.eigenValues{1});
 imagEigenValues = imag(plotData.eigenValues{1});
 scatter(imagEigenValues, -realEigenValues, 20, realEigenValues, 'filled')
@@ -137,7 +129,9 @@ set(gca, 'XScale', 'log')
 set(gca, 'YScale', 'log')
 grid on
 box on
-slopeLine('loglog' ,2, [1E-1,], 1E-3, 'TextLocation', [8E-4, 1])
+slopeLine('loglog' ,2, [5E-2,1E-1], 2E-4, 'TextLocation', [7E-2, 1.5E-4])
+slopeLine('loglog' ,.5, [1E-1,5E-1], 7E-4, 'TextLocation', [3E-1, 6E-4])
+slopeLine('loglog' ,.8, [5E-1,2], 1.5E-3, 'TextLocation', [1, 1E-3])
 
 %% damped sandbox
 matMass = eye(2)
