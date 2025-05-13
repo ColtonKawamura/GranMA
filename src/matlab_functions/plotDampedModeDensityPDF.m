1792f9123219a8cd782fc42109077d595f151dcc
Colton Kawamura <c.kawamura@me.com>
2025-05-12 20:25:06 -0400

    Added Abe's scaling.

--- src/matlab_functions/plotDampedModeDensityPDF.m
+++ src/matlab_functions/plotDampedModeDensityPDF.m
@@ -10,7 +10,9 @@ function plotDampedModeDensityPDF(data, pressure_list, damping_list)
 % Note: usually you want multiple presssures and a single damping constant
 % More note: this assumes that the eigenValues = damping + i frequency
 % plotDampedModeDensityPDF("out/2d_damped_eigenStuff/2D_damped_eigenstuff_N400_K100_M1.mat", [.2, .01, .001], [.1])
-figure
+figure(1), clf
+figure(2), clf
+
 for i = 1:length(pressure_list)
     pressure = pressure_list(i);
 
@@ -21,11 +23,14 @@ for i = 1:length(pressure_list)
         dataPressureDamping = filterData(data, 'pressure', pressure,  'damping', damping_constant);
         eigenValues = dataPressureDamping.eigenValues{1};
         keepIdx = imag(eigenValues) > 0; % keep the positive eigenvalues
+        dataPressureDamping.pressure
+        sum(keepIdx)
         % eigenValues = abs(imag(eigenValues(keepIdx))) % imag part carries the frequency, abs() because QZ sovler does weird things
         Lx = dataPressureDamping.Lx(1);
         Ly = dataPressureDamping.Ly(1);
 
         [edges, normalized_counts] = modeDensity(abs(imag(eigenValues)), "damped",true);
+
         % --- Verification Step ---
         bin_widths = diff(edges); % Calculate the width of each bin (Length N)
         num_bins = length(bin_widths);
@@ -45,11 +50,23 @@ for i = 1:length(pressure_list)
         else
             markerSize = exp(dataPressureDamping.damping/max(damping_list))*3;
         end
+        pressureValue = dataPressureDamping.pressure;
         binCenters = sqrt( edges(1:end-1) .* edges(2:end) ); 
         pressureLabel = sprintf('$ %.4f, %.4f $', dataPressureDamping.pressure, dataPressureDamping.damping); 
+        gammaHalf = .5*damping_constant;
+        omegaKnee = sqrt(pressureValue)*sqrt(1-gammaHalf^2);
+
+        figure(1)
+        loglog(binCenters/omegaKnee^(1), normalized_counts/omegaKnee^0, '-o', 'MarkerSize', markerSize, 'MarkerFaceColor', marker_color, 'MarkerEdgeColor', marker_color, 'Color', marker_color, 'DisplayName', pressureLabel);
+        xlabel('$\omega/\omega_d$', 'Interpreter', 'latex', 'FontSize', 20)
+        ylabel('$D(\omega)/\omega_d^{1/4}$', 'Interpreter', 'latex', 'FontSize', 20)
+        hold on
+
+        figure(2)
         plot(binCenters, normalized_counts, '-o', 'MarkerSize', markerSize, 'MarkerFaceColor', marker_color, 'MarkerEdgeColor', marker_color, 'Color', marker_color, 'DisplayName', pressureLabel);
         xlabel('$\omega$', 'Interpreter', 'latex', 'FontSize', 20)
         ylabel('$D(\omega)$', 'Interpreter', 'latex', 'FontSize', 20)
+
         title(sprintf('$L_x$ by $L_y$: %.2f by %.2f', Lx, Ly), 'Interpreter', 'latex', 'FontSize', 16);
         set(gca, "XScale", "log")
         set(gca, "YScale", "log")
